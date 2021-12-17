e8fe54e57df297594800458c6021087c93bb27c1
Ahmet YAVUZ <1yavuzahmet@gmail.com>
2021-12-17 19:38:01 +0300

    Add files via upload

---
+++ HackerRankDay1DataTypes/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int i1=scan.nextInt();
+        scan.nextLine();
+        double d1=scan.nextDouble();
+        scan.nextLine();
+        String s1=scan.nextLine();
+
+        System.out.println(i+i1);
+        System.out.println(d+d1);
+        System.out.println(s+s1);
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
+}
