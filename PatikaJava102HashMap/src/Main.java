9d441f418e127e3051e06c39b93904920c4f448c
Ahmet YAVUZ <1yavuzahmet@gmail.com>
2021-12-20 13:21:21 +0300

    Add files via upload

---
+++ PatikaJava102HashMap/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+
+        capitalCities.get("England");
+        capitalCities.remove("England");
+        capitalCities.clear();
+        capitalCities.size();
+
+        for (String i : capitalCities.keySet()) {
+            System.out.println(i);
+        }
+
+        for (String i : capitalCities.values()) {
+            System.out.println(i);
+        }
+
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+    }
+}
