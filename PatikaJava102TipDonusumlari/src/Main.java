a628f895fe8c945ec1408d726624459ba1bf397f
Ahmet YAVUZ <1yavuzahmet@gmail.com>
2022-01-14 12:12:11 +0300

    Add files via upload

---
+++ PatikaJava102TipDonusumlari/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int a=20;
+    String str=String.valueOf(a);
+        System.out.println(str);
+        int b=Integer.parseInt(str);
+        System.out.println(b+a);
+    }
+}
