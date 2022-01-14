4382eb27bf2ba96f24d84d6a6f27fb02adb93c76
Ahmet YAVUZ <1yavuzahmet@gmail.com>
2022-01-14 12:05:01 +0300

    Add files via upload

---
+++ ProgrammingWithMoshImplemention/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var editor = new Editor();
+        var history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
+
+    }
+}
