e6dd0a2eeecd984681285f3d7b421ae2fc8322b3
Ahmet YAVUZ <1yavuzahmet@gmail.com>
2022-01-17 22:40:46 +0300

    Add files via upload

---
+++ PatikaJava102EnumClasses/src/Day.java
@@ -0,0 +1,21 @@
+public enum Day {
+    PAZARTESİ(1),
+    SALI(2),
+    CARSAMBA(3),
+    PERSEMBE(4),
+    CUMA(5),
+    CUMARTESI(6),
+    PAZAR(7);
+    private int day;
+    Day(int day){
+        this.day=day;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+}
