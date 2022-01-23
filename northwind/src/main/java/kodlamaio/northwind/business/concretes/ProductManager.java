6b918401ae42e5181bc0376b809800dc443c6ea5
Ahmet YAVUZ <1yavuzahmet@gmail.com>
2022-01-23 16:57:03 +0300

    Add files via upload

---
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,29 @@
+package kodlamaio.northwind.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.dataaccess.abstracts.ProductDao;
+import kodlamaio.northwind.entities.concretes.Product;
+
+@Service
+public class ProductManager implements ProductService {
+
+	private ProductDao productDao;
+
+	@Autowired
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	@Override
+	public List<Product> getAll() {
+
+		return productDao.findAll();
+	}
+
+}
